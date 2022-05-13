c5883579944b3d56e574c8a81f54e9397477411c
Mariia Kiliushina <markil@Mariias-MacBook-Air.local>
2022-05-13 21:23:12 +0400

    init: start learn-english-game

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+const App: FC = () => {
+  return (
+    <>
+      <div>SASHA PROSTOKVASHA</div>
+    </>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+let config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist/"),
+    filename: "index_bundle.js",
+  },
+  devServer: {
+    port: 8001,
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        use: ["babel-loader"],
+        exclude: "/node-modules",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  // devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+};
+
+module.exports = config;
