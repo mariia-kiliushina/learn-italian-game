731e6233b94b1d3b2f6deeed953d301913668f01
Mariia Kiliushina <markil@Mariias-MacBook-Air.local>
2022-06-06 14:41:15 +0400

    before patch

--- src/components/WelcomingPage/index.tsx
+++ src/components/WelcomingPage/index.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
 
 import italianMan from '../../../public/images/italian-man.png'
-import Card from './Card'
 import styles from './index.module.scss'
 
 const WelcomingPage: FC = () => {
@@ -11,8 +10,6 @@ const WelcomingPage: FC = () => {
         <h2>You can Learn la bella Italian language</h2>
         <p>playing</p>
         <img src={italianMan} height="280px" />
-        <Card />
-        <Card />
       </div>
     </div>
   )
