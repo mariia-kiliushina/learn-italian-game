a2a23140d4065e0e7a5c8aaecea972bd936bcf83
Mariia Kiliushina <markil@Mariias-MacBook-Air.local>
2022-06-10 18:04:04 +0400

    fix: minor fixes

--- declarations.d.ts
+++ declarations.d.ts
@@ -4,6 +4,3 @@ declare module '*.png' {
   const content: any
   export default content
 }
-// declare module '*.svg' {
-//   const content: any
-// }
--- src/components/WelcomingPage/Card/index.tsx
+++ src/components/WelcomingPage/Card/index.tsx
@@ -9,12 +9,12 @@ type Props = {
   className: string
 }
 
-const Card: FC<Props> = ({ heading, description, imgSrc, className }) => {
+const Card: FC<Props> = ({ heading, description, imageSrc, className }) => {
   return (
     <div className={className}>
       <div className={styles.cardItemWrapper}>
         <div className={styles.cardImageWrapper}>
-          <img src={imgSrc} />
+          <img src={imageSrc} />
         </div>
         <div className={styles.cardTextWrapper}>
           <p className={styles.cardHeader}>{heading}</p>
